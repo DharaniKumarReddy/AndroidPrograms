cadcb1316fb90aa2c1ec6c84cc5a260574e74761
hemanthgaddam <hemanth.6224@gmail.com>
2015-12-18 22:11:50 +0530

    added textview

--- app/src/main/java/com/dharani/emids/logicalprogramscalc/HemanthActivity.java
+++ app/src/main/java/com/dharani/emids/logicalprogramscalc/HemanthActivity.java
@@ -4,10 +4,11 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class HemanthActivity extends ActionBarActivity {
-
+    TextView t;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
