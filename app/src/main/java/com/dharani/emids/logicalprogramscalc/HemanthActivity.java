c7325ecf0a6bcbff1249616f922155239f8a74bb
hemanthgaddam <hemanth.6224@gmail.com>
2015-12-18 21:35:02 +0530

    added line

--- app/src/main/java/com/dharani/emids/logicalprogramscalc/HemanthActivity.java
+++ app/src/main/java/com/dharani/emids/logicalprogramscalc/HemanthActivity.java
@@ -13,6 +13,7 @@ public class HemanthActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hemanth);
+        
     }
 
 
